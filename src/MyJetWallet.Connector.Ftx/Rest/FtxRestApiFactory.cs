7e0bd228f6876d706257aa9637c8813f0eff0d72
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-04-12 16:28:03 +0300

    add factory for rest api

---
+++ src/MyJetWallet.Connector.Ftx/Rest/FtxRestApiFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.PortableExecutable;
+using FtxApi;
+
+namespace MyJetWallet.Connector.Ftx.Rest
+{
+    public static class FtxRestApiFactory
+    {
+        public static FtxRestApi CreateClient(string apiKey, string apiSecret)
+        {
+            var client = new Client(apiKey, apiSecret);
+            var api = new FtxRestApi(client);
+
+            return api;
+        }
+    }
+}
